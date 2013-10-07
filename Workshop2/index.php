7c23836ef1548ab087cf35cc57c98d6523873256
Kriss Magnusson <krissm@gmail.com>
2013-10-07 10:42:09 +0700

    to do

--- Workshop2/index.php
+++ Workshop2/index.php
@@ -5,13 +5,16 @@ $controller = new Controller();
 
 $controller->Event();
 
-
-/*
-if (isset($_POST['AddNewMember'])){
-	require_once 'View/Member.php';
-}
-
-if (isset($_POST['AddMember'])){
-	$membershipController->AddMember($_POST['name'], $_POST['pn']);
-}
-*/
+//TODO: add member and boat objects. connects boats to members through association rather than keys
+//TODO: test for wrong input
+//TODO:	add communcation diagrams to the application (over the two different view as well)
+//TODO: add class diagram to the application
+//TODO: add link to github in the application to get access to the source code
+//TODO: put it on the bht server
+//TODO: add no of boats to compact view
+//TODO: type of boat enumerable?
+//TODO: no code duplication
+//TODO: login - user needs to be logged in to create, update, and delete information (members and boats). Anonymos users should still be able to view all info
+//TODO: search - use a design pattern ; show what needs to change in order to add a new search criterion (you don't have to implement)
+//TODO: search - use a design pattern for describing multiple search criterions
+//TODO: data validation - messages to the user  
